142e3e7a0899c694a96fca04d793ebd7096a1243
rupl <rupl@411999.no-reply.drupal.org>
2018-06-22 09:04:00 +0200

    Issue #2914178 by rupl: Wait until window.onload to register Service Worker

--- js/serviceworker-load.js
+++ js/serviceworker-load.js
@@ -1,4 +1,4 @@
-(function (Drupal, navigator, window) {
+(function (Drupal, navigator, window, $) {
 
   'use strict';
 
@@ -6,11 +6,31 @@
     return;
   }
 
-  navigator.serviceWorker.register(Drupal.settings.pwa.path, {scope: Drupal.settings.basePath})
-    .then(function () { })
+  /**
+   * Helper function to register Service Worker. Registration occurrs during
+   * different events according to admin configuration.
+   */
+  function pwaServiceWorkerRegister() {
+    navigator.serviceWorker
+    .register(Drupal.settings.pwa.path, {scope: Drupal.settings.basePath})
+    .then(function () {
+
+    })
     .catch(function (error) {
       // Something went wrong.
     });
+  }
+
+  // Read Drupal.settings and register SW during the desired event.
+  if (Drupal.settings.pwa.registrationEvent === 'immediate') {
+    pwaServiceWorkerRegister();
+  }
+  else if (Drupal.settings.pwa.registrationEvent === 'documentready') {
+    $(document).ready(pwaServiceWorkerRegister);
+  }
+  else {
+    window.addEventListener('load', pwaServiceWorkerRegister);
+  }
 
   // Reload page when user is back online on a fallback offline page.
   document.body.addEventListener('online', function () {
@@ -30,4 +50,4 @@
     });
   /**/
 
-}(Drupal, navigator, window));
+}(Drupal, navigator, window, jQuery));
